3b285d3e52e03ad8e0089fb9945e8cfb0db210b9
apcel <apcel@yandex.ru>
2016-10-17 19:04:15 +0300

    Started writing of laboratory core

---
+++ MAKSIMOV_LR2/core.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    19:00:36 10/17/2016 
+// Design Name: 
+// Module Name:    core 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module core(
+    input hit,
+    input clr,
+    output [3:0] NOM,
+    output [1:0] BIT,
+    output [3:0] LE,
+    output TX
+    );
+wire [3:0] rg_a;
+//wire ;
+
+
+
+endmodule
