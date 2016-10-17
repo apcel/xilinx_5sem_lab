8136c288b7dd9d7fc301abb19fc1e8405f5a3914
apcel <apcel@yandex.ru>
2016-10-17 20:01:59 +0300

    Started to implement hex-to-seg driver

---
+++ MAKSIMOV_LR2/HexToLed.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    19:55:16 10/17/2016 
+// Design Name: 
+// Module Name:    HexToLed 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module HexToLed(
+    input [3:0] x,
+    output [7:0] y
+    );
+
+
+endmodule
